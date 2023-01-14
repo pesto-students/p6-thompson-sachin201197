f44193324223310eaf22b3bdf2ee9f97a66bcd00
sachin <sachin@gmail.com>
2023-01-14 20:59:00 +0530

    new commit to the newly crwated branch week10

---
+++ Week-10/urlshortner/src/Pages/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Header from "../Header";
+function About() {
+  return (
+    <div className="about">
+      <Header></Header>
+      <h2>About Us</h2>
+      <p>
+        <h5>What Is An About Us Page?</h5> An About Us Page is a page on your
+        website that tells your readers all about you. It includes a detailed
+        description covering all aspects of your business and you as an
+        entrepreneur. This can include the products or services you are
+        offering, how you came into being as a business, your mission and
+        vision, your aim, and maybe something about your future goals too. Your
+        About Us page is your perfect opportunity to tell a compelling story
+        about your business. Even though an About Us page is one of the most
+        important elements of a website, it is often one of the most overlooked
+        elements. Compared to a landing page, an About Us page help you
+        communicate a range of things:
+      </p>
+      <p>
+        <h5>History</h5> Take the visitors on your website to a trip down memory
+        lane, and give them an insight to the history behind your store. Here,
+        you can show them where, how, and when you started, and everything your
+        business has accomplished on the way. This is your chance to share your
+        relevant milestones and achievements relating to your business in an
+        engaging way. You can even choose to present your history to your
+        viewers in the form of a timeline, which allows you to display a large
+        amount of information in a visually engaging manner. Your customers or
+        potential customers might be interested in seeing how you grew over the
+        years.
+      </p>
+      <p>
+        <h5>Team Member</h5> Profiles Add an emotional element to your About Us
+        page by adding details of the people that are driving the passion at
+        your business. People find it easier to connect with human beings, so
+        allow the personality of your crew to shine through the About Us page.
+      </p>
+      <p>
+        <h5>Multimedia & Infographics</h5>A well-built infographic might help
+        visitors remember about you or your business better than any amount of
+        words. So if you think it works better for your brand or business, skip
+        the lengthy description, or add a well-designed graphic that can help
+        your visitors understand your concept. Or maybe you would like to add a
+        cool video about how your business was created, or about your story and
+        let it speak for itself.
+      </p>
+    </div>
+  );
+}
+
+export default About;
